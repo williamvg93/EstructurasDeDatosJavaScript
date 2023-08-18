82342e2e335e0f388d1aa5ce0ca0a9556b700869
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-08-18 07:07:39 -0500

    Update lista.js

--- TutorialEstructuraLista/structList.js
+++ TutorialEstructuraLista/structList.js
@@ -7,14 +7,21 @@ class List {
     push(data) {
         let node = new Node(data)
         /* a la propiedad head solo se le va a greagr el valor del primer elemento agregado, por eso se utiliza el if, como la propiedad se inicializa en null, esta comprobacion siempre se va a se true cuando se realice el primer push, despues nunca se volvera a ejecutar */
-        this.last = node
         if (this.head == null) {
             this.head = node
         } else {
             this.last.next = node
         }
+        this.last = node
     }
 
+    print() {
+        let aux = this.head
+        while (aux != null) {
+            console.log(aux.data)
+            aux = aux.next
+        }
+    }
 }
 
 class Node {
@@ -23,3 +30,9 @@ class Node {
         this.next = null
     }
 }
+
+let mylist = new List()
+mylist.push(1)
+mylist.push(3)
+mylist.push(10)
+mylist.push(16)
